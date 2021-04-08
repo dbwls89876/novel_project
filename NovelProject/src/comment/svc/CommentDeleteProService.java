6bbc1b9eb6380ce625eda65f68fa01db3d83bc56
yn-s02 <yn-s02@DESKTOP-IANBDCO>
2021-04-08 14:22:07 +0900

    혜린_0408_5교시

---
+++ NovelProject/src/comment/svc/CommentDeleteProService.java
@@ -0,0 +1,17 @@
+package comment.svc;
+
+import static db.JdbcUtil.*;
+
+public class CommentDeleteProService {
+
+	public boolean isArticleWriter(int commentID, String parameter) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean removeArticle(int commentID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- NovelProject/src/controller/CommentController.java
+++ NovelProject/src/controller/CommentController.java
@@ -11,18 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
 import board.action.*;
+import comment.action.*;
 import vo.ActionForward;
+
 /**
  * Servlet implementation class BoardFrontController
  */
-@WebServlet("*.bo")
-public class BoardFrontController extends HttpServlet {
+@WebServlet("*.comm")
+public class CommentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BoardFrontController() {
+    public CommentController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -50,53 +52,53 @@ public class BoardFrontController extends HttpServlet {
 		ActionForward forward=null;
 		Action action=null;
 		
-		if(command.equals("/boardWriteForm.bo")) {
+		if(command.equals("/commentWriteForm.comm")) {
 			forward = new ActionForward();
-			forward.setPath("board/boardWrite.jsp");
-		}else if(command.equals("/boardWritePro.bo")) {
-			action = new BoardWriteProAction();
+			forward.setPath("board/boardView.jsp");
+		}else if(command.equals("/commentWritePro.comm")) {
+			action = new CommentWriteProAction();
 			try {
 				forward = action.execute(request, response);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-		}else if(command.equals("/boardList.bo")) {
-			action = new BoardListAction();
+		}else if(command.equals("/commentList.comm")) {
+			action = new CommentListAction();
 			try {
 				forward = action.execute(request, response);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-		}else if(command.equals("/boardDetail.bo")) {
-			action = new BoardDetailAction();
+		}else if(command.equals("/commentDetail.comm")) {
+			action = new CommentDetailAction();
 			try {
 				forward = action.execute(request, response);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-		}else if(command.equals("/boardModifyForm.bo")) {
-			action = new BoardModifyFormAction();
+		}else if(command.equals("/commentModifyForm.comm")) {
+			action = new CommentModifyFormAction();
 			try {
 				forward = action.execute(request, response);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-		}else if(command.equals("/boardModifyPro.bo")) {
-			action = new BoardModifyProAction();
+		}else if(command.equals("/commmentModifyPro.comm")) {
+			action = new CommentModifyProAction();
 			try {
 				forward = action.execute(request, response);
 			}catch (Exception e) {
 				e.printStackTrace();
 			}
-		}else if(command.equals("/boardDeleteForm.bo")) {
+		}else if(command.equals("/commentDeleteForm.comm")) {
 			String nowPage = request.getParameter("page");
 			request.setAttribute("page", nowPage);
-			int boardID = Integer.parseInt(request.getParameter("boardID"));
-			request.setAttribute("boardID", boardID);
+			int boardID = Integer.parseInt(request.getParameter("commentID"));
+			request.setAttribute("commentID", commentID);
 			forward = new ActionForward();
-			forward.setPath("/board/boardDelete.jsp");
-		}else if (command.contentEquals("/boardDeletePro.bo")) {	
-			action = new BoardDeleteProAction();
+			forward.setPath("/board/boardView.jsp");
+		}else if (command.contentEquals("/commentDeletePro.comm")) {	
+			action = new CommentDeleteProAction();
 			try {
 				forward = action.execute(request, response);
 			}catch(Exception e) {
